1b7bf5003ba296a705cf83213ae5179f63946061
Johnny Sparks <johnnymsparks@gmail.com>
2017-09-13 18:09:04 -0700

    Carthage ready streaming kit

---
+++ StreamingKit/StreamingKit iOS/StreamingKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  StreamingKit_iOS.h
+//  StreamingKit iOS
+//
+//  Created by John Sparks on 9/13/17.
+//  Copyright © 2017 Thong Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StreamingKit_iOS.
+FOUNDATION_EXPORT double StreamingKit_iOSVersionNumber;
+
+//! Project version string for StreamingKit_iOS.
+FOUNDATION_EXPORT const unsigned char StreamingKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StreamingKit_iOS/PublicHeader.h>
+
+
