861c1c85994b17cacfab8d45e88f9221e0a53371
Johnny Sparks <johnnymsparks@gmail.com>
2017-09-13 18:22:06 -0700

    Trying agin w/ some real headers

--- StreamingKit/StreamingKit iOS/StreamingKit_iOS.h
+++ StreamingKit/StreamingKit iOS/StreamingKit_iOS.h
@@ -16,4 +16,5 @@ FOUNDATION_EXPORT const unsigned char StreamingKit_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <StreamingKit_iOS/PublicHeader.h>
 
+#import <StreamingKit_iOS/STKAudioPlayer.h>
 
